3fc1a5c6a0210317737578cb98b0bdbdc78e5eb5
Gerrinator <vangenderengerard@gmail.com>
2025-03-15 23:08:47 -0600

    Added simple card movement system

---
+++ ElJuegoDelGambometer/Assets/Scripts/Card.cs
@@ -0,0 +1,206 @@
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+using System.Collections.Generic;
+using System.Collections;
+using UnityEngine.UI;
+using System.Reflection;
+
+// Largely borrowed from Mix and Jam's "Balatro Feel" video
+// https://www.youtube.com/watch?v=I1dAZuWurw4&ab_channel=MixandJam
+
+[System.Serializable]
+public class CardUnityEvent : UnityEvent<Card> { }
+[System.Serializable]
+public class CardBoolUnityEvent : UnityEvent<Card, bool> { }
+
+public class Card : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndDragHandler, IPointerEnterHandler, IPointerExitHandler, IPointerUpHandler, IPointerDownHandler
+{
+    private Canvas canvas;
+    private Image imageComponent;
+    [SerializeField] private bool instantiateVisual = true;
+    private VisualCardsHandler visualHandler;
+    private Vector3 offset;
+
+    [Header("Movement")]
+    [SerializeField] private float moveSpeedLimit = 50;
+
+    [Header("Selection")]
+    public bool selected;
+    public float selectionOffset = 50;
+    private float pointerDownTime;
+    private float pointerUpTime;
+
+    [Header("Visual")]
+    [SerializeField] private GameObject cardVisualPrefab;
+    [HideInInspector] public CardVisual cardVisual;
+
+    [Header("States")]
+    public bool isHovering;
+    public bool isDragging;
+    [HideInInspector] public bool wasDragged;
+
+    [Header("Events")]
+    [HideInInspector] public CardUnityEvent PointerEnterEvent;
+    [HideInInspector] public CardUnityEvent PointerExitEvent;
+    [HideInInspector] public CardBoolUnityEvent PointerUpEvent;
+    [HideInInspector] public CardUnityEvent PointerDownEvent;
+    [HideInInspector] public CardUnityEvent BeginDragEvent;
+    [HideInInspector] public CardUnityEvent EndDragEvent;
+    [HideInInspector] public CardBoolUnityEvent SelectEvent;
+
+    void Start()
+    {
+        if (PointerEnterEvent == null) PointerEnterEvent = new CardUnityEvent();
+        if (PointerExitEvent == null) PointerExitEvent = new CardUnityEvent();
+        if (PointerUpEvent == null) PointerUpEvent = new CardBoolUnityEvent();
+        if (PointerDownEvent == null) PointerDownEvent = new CardUnityEvent();
+        if (BeginDragEvent == null) BeginDragEvent = new CardUnityEvent();
+        if (EndDragEvent == null) EndDragEvent = new CardUnityEvent();
+        if (SelectEvent == null) SelectEvent = new CardBoolUnityEvent();
+        
+        canvas = GetComponentInParent<Canvas>();
+        imageComponent = GetComponent<Image>();
+
+        if (!instantiateVisual)
+            return;
+
+        visualHandler = FindObjectOfType<VisualCardsHandler>();
+        cardVisual = Instantiate(cardVisualPrefab, visualHandler ? visualHandler.transform : canvas.transform).GetComponent<CardVisual>();
+        cardVisual.Initialize(this);
+    }
+
+    void Update()
+    {
+        ClampPosition();
+
+        if (isDragging)
+        {
+            Vector2 targetPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition) - offset;
+            Vector2 direction = (targetPosition - (Vector2)transform.position).normalized;
+            Vector2 velocity = direction * Mathf.Min(moveSpeedLimit, Vector2.Distance(transform.position, targetPosition) / Time.deltaTime);
+            transform.Translate(velocity * Time.deltaTime);
+        }
+    }
+
+    void ClampPosition()
+    {
+        Vector2 screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
+        Vector3 clampedPosition = transform.position;
+        clampedPosition.x = Mathf.Clamp(clampedPosition.x, -screenBounds.x, screenBounds.x);
+        clampedPosition.y = Mathf.Clamp(clampedPosition.y, -screenBounds.y, screenBounds.y);
+        transform.position = new Vector3(clampedPosition.x, clampedPosition.y, 0);
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        BeginDragEvent.Invoke(this);
+        Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        offset = mousePosition - (Vector2)transform.position;
+        isDragging = true;
+        canvas.GetComponent<GraphicRaycaster>().enabled = false;
+        imageComponent.raycastTarget = false;
+
+        wasDragged = true;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        EndDragEvent.Invoke(this);
+        isDragging = false;
+        canvas.GetComponent<GraphicRaycaster>().enabled = true;
+        imageComponent.raycastTarget = true;
+
+        StartCoroutine(FrameWait());
+
+        IEnumerator FrameWait()
+        {
+            yield return new WaitForEndOfFrame();
+            wasDragged = false;
+        }
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        PointerEnterEvent.Invoke(this);
+        isHovering = true;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        PointerExitEvent.Invoke(this);
+        isHovering = false;
+    }
+
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        if (eventData.button != PointerEventData.InputButton.Left)
+            return;
+
+        PointerDownEvent.Invoke(this);
+        pointerDownTime = Time.time;
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        if (eventData.button != PointerEventData.InputButton.Left)
+            return;
+
+        pointerUpTime = Time.time;
+
+        PointerUpEvent.Invoke(this, pointerUpTime - pointerDownTime > .2f);
+
+        if (pointerUpTime - pointerDownTime > .2f)
+            return;
+
+        if (wasDragged)
+            return;
+
+        selected = !selected;
+        SelectEvent.Invoke(this, selected);
+
+        if (selected)
+            transform.localPosition = new Vector3(0, selectionOffset, 0);
+        else
+            transform.localPosition = Vector3.zero;
+    }
+
+    public void Deselect()
+    {
+        if (selected)
+        {
+            selected = false;
+            if (selected)
+                transform.localPosition = new Vector3(0, selectionOffset, 0);
+            else
+                transform.localPosition = Vector3.zero;
+        }
+    }
+
+
+    public int SiblingAmount()
+    {
+        return transform.parent.CompareTag("Slot") ? transform.parent.parent.childCount - 1 : 0;
+    }
+
+    public int ParentIndex()
+    {
+        return transform.parent.CompareTag("Slot") ? transform.parent.GetSiblingIndex() : 0;
+    }
+
+    public float NormalizedPosition()
+    {
+        return transform.parent.CompareTag("Slot") ? ExtensionMethods.Remap((float)ParentIndex(), 0, (float)(transform.parent.parent.childCount - 1), 0, 1) : 0;
+    }
+
+    private void OnDestroy()
+    {
+        if(cardVisual != null)
+        Destroy(cardVisual.gameObject);
+    }
+}
